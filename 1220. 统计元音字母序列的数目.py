ec9c183d4b0c06659b383ff6387940939b35ddae
dark4scope <mail.xgwang@gmail.com>
2023-05-16 01:41:54 +0800

    准备机试3

---
+++ 1220. 统计元音字母序列的数目.py
@@ -0,0 +1,12 @@
+import numpy as np
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        factor = np.mat([(0, 1, 0, 0, 0), (1, 0, 1, 0, 0), (1, 1, 0, 1, 1), (0, 0, 1, 0, 1), (1, 0, 0, 0, 0)], np.dtype('O'))
+        res = np.mat([(1, 1, 1, 1, 1)], np.dtype('O'))
+        n -= 1
+        while n > 0:
+            if n % 2 == 1:
+                res = res * factor % 1000000007
+            factor = factor * factor % 1000000007
+            n = n // 2
+        return res.sum() % 1000000007
---
+++ 354. 俄罗斯套娃信封问题.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        if not envelopes:
+            return 0
+        
+        n = len(envelopes)
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        print(envelopes)
+        f = [envelopes[0][1]]
+        for i in range(1, n):
+            if (num := envelopes[i][1]) > f[-1]:
+                f.append(num)
+            else:
+                index = bisect.bisect_left(f, num)
+                f[index] = num
+        
+        return len(f)
